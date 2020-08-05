e866a8999735050887c1bcdc1c5a1fa2574f7310
Suizhazha <573505403@qq.com>
2020-08-05 21:56:48 +0800

    完成整体布局的CSS

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,11 @@ import { createApp } from 'vue'
 // @ts-ignore
 import App from "./App.vue"
 import './index.scss'
-
-// @ts-ignore
-import Home from './views/Home.vue'
-
-// @ts-ignore
-import Doc from './views/Doc.vue'
-
-
-
-//创建hash型路由，还有创建内存型路由和history型路由
-
-import { createWebHashHistory, createRouter } from 'vue-router'
-
-const history = createWebHashHistory()
-
-//createRouter函数接收options: RouterOptions
-//RouterOptions又包括history和router
-const router = createRouter({
-    history,
-    routes: [
-        { path: '/', component: Home },
-        { path: '/doc', component: Doc }
-    ]
-
-})
-
-
+import {router} from './router';
 
 const app = createApp(App)
 
 //使用路由
 app.use(router)
 app.mount('#app')
+

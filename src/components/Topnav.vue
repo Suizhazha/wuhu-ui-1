ef9d99ac2f375185e79898f28315dea4e1835460
Suizhazha <573505403@qq.com>
2020-08-05 16:24:11 +0800

    Topnav组件封装完成

---
+++ src/components/Topnav.vue
@@ -0,0 +1,31 @@
+<template>
+        <div class="top-nav">
+            <div class="logo">LOGO</div>
+            <ul class="menu">
+                <li>菜单1</li>
+                <li>菜单2</li>
+            </ul>
+        </div>
+</template>
+<style lang="scss" scoped>
+    .top-nav {
+        background: lightblue;
+        display: flex;
+        padding: 16px;
+
+        > .logo {
+            max-width: 6em;
+            margin-right: auto;
+        }
+
+        > .menu {
+            display: flex;
+            white-space: nowrap;
+            flex-wrap: nowrap;
+
+            > li {
+                margin: 0 1em;
+            }
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,6 @@
 <template>
     <div>
-        <div class="top-nav">
-            <div class="logo">LOGO</div>
-            <ul class="menu">
-                <li>菜单1</li>
-                <li>菜单2</li>
-            </ul>
-        </div>
-
+        <Topnav/>
         <div class="banner">
             <h1>芜湖UI</h1>
             <h2>基于Vue3.0的UI框架</h2>
@@ -17,27 +10,13 @@
             </p>
         </div>
     </div>
+
 </template>
 
-<style lang="scss" scoped>
-    .top-nav {
-        background: lightblue;
-        display: flex;
-        padding: 16px;
 
-        > .logo {
-            max-width: 6em;
-            margin-right: auto;
-        }
-
-        > .menu {
-            display: flex;
-            white-space: nowrap;
-            flex-wrap: nowrap;
-
-            > li {
-                margin: 0 1em;
-            }
-        }
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+    export default {
+components:{Topnav}
     }
-</style>
+</script>

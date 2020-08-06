a54edb0b574a44bd21b401539447801786100c09
Suizhazha <573505403@qq.com>
2020-08-06 19:57:33 +0800

    使用v-model

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -2,24 +2,24 @@
     <button @click="toggle" :class="{checked:value}">
         <span></span>
     </button>
-    <div>{{value}}</div>
+
 </template>
 
 <script lang="ts">
-import {ref} from 'vue'
+  import {ref} from 'vue';
 
   export default {
-      props:{
-        value:Boolean
-      },
-    setup(props,context){
-      const toggle =()=>{
+    props: {
+      value: Boolean
+    },
+    setup(props, context) {
+      const toggle = () => {
 
-        context.emit('input',!props.value)
+        context.emit('update:value', !props.value);
+        //this.$emit()
+      };
 
-      }
-
-      return {toggle}
+      return {toggle};
     }
   };
 </script>
@@ -27,32 +27,52 @@ import {ref} from 'vue'
 <style lang="scss" scoped>
     $h: 22px;
     $h2: $h - 4px;
-    button{
+    button {
         height: $h;
         width: $h*2;
         border: none;
         background: grey;
         border-radius: $h/2;
         position: relative;
+
+        > span {
+            position: absolute;
+            top: 2px;
+            left: 2px;
+            height: $h2;
+            width: $h2;
+            background: white;
+            border-radius: $h2 / 2;
+            transition: all 250ms; //通用秒数，也很好记
+        }
+
+        &.checked {
+            background: #1890ff;
+
+            > span {
+                left: calc(100% - #{$h2} - 2px);
+            }
+        }
+
+        //取消button外黑框
+        &:focus {
+            outline: none;
+        }
+
+        &:active {
+            > span {
+                width: $h2 + 4px;
+            }
+        }
+
+        &.checked:active {
+            > span {
+                width: $h2 +4px;
+                margin-left: -4px;
+            }
+        }
+
     }
-    span{
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        height: $h2;
-        width: $h2;
-        background: white;
-        border-radius: $h2/2;
-        transition: left 250ms ;//通用秒数，也很好记
-    }
-    button.checked{
-        background: blue;
-    }
-    button.checked > span{
-        left: calc(100% - #{$h2} - 2px);
-    }
-    //取消button外黑框
-    button:focus{
-        outline: none;
-    }
+
+
 </style>

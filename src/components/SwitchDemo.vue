3ea905361006f88a49f14f33f2858d01b0d774b6
Suizhazha <573505403@qq.com>
2020-08-09 21:41:36 +0800

    Switch组件基本完成

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,24 +1,26 @@
 <template>
     <div>
-  <!--value不仅仅是初始状态，还可以表示更新后的状态-->
+        <!--value不仅仅是初始状态，还可以表示更新后的状态-->
         <!--:value是变量，value是字符串 -->
         <!--@input对应Switch组件中的emit中的接收的第一个事件，
        $event对应emit中的!props.value -->
+        <!--<Switch :value = "y" @update:value=" y = $event" /> -->
 
-<Switch :value = "y" @input=" y = $event" />
+        <Switch v-model:value="bool"/>
     </div>
 </template>
 
 <script lang="ts">
-    import Switch from '../lib/Switch.vue';
-    import { ref } from 'vue'
+  import Switch from '../lib/Switch.vue';
+  import {ref} from 'vue';
+
   export default {
     components: {Switch},
-    setup(){
-      const y = ref(false)
-      return {y}
+    setup() {
+      const bool = ref(false);
+      return {bool};
     }
-  }
+  };
 </script>
 
 <style lang="scss" scoped>

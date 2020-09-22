ba40f431f8cdcd60acccae33f951cdb15f990034
Suizhazha <573505403@qq.com>
2020-09-22 20:33:29 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,4 +2,18 @@
     <div>
        button文档
     </div>
+  <h1>
+    示例1
+  </h1>
+    <Button>按钮</Button>
 </template>
+
+<script lang="ts">
+
+
+import Button from '../lib/Button.vue';
+
+export default {
+components:{Button:Button}
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+  <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Button.vue'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

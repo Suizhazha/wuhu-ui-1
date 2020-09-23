fc55b966dfd357691e87a2eafb75cb4140ed324d
Suizhazha <573505403@qq.com>
2020-09-23 20:40:34 +0800

    渲染Tabs及Tab内容

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-Tab组件
+<slot />
   </div>
 </template>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-Tabs组件
+    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+    <component v-for="(c,index) in defaults" :key="index" :is="c" />
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Tabs.vue'
-};
+  name: 'Tabs.vue',
+  setup(props,context){
+    const defaults = context.slots.default()
+    const titles = defaults.map((tab)=>{
+      return tab.props.title
+    })
+
+    return {defaults,titles}
+  }
+
+}
 </script>
 
 <style lang="scss" scoped>

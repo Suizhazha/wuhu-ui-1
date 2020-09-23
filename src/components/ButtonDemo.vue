b29d320e26ec3304ee0e9de1c53c7f9d490e7839
Suizhazha <573505403@qq.com>
2020-09-23 15:15:00 +0800

    让button支持link和level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -11,6 +11,45 @@
       <Button theme="link ">你好</Button>
       <Button theme="text">你好</Button>
     </div>
+  <h1>
+    示例2
+  </h1>
+    <div>
+      <div>
+        <Button size="big">大大大</Button>
+         <Button>普普通</Button>
+        <Button size="small">小小小</Button>
+      </div>
+      <div>
+        <Button theme="link" size="big">大大大</Button>
+        <Button theme="link">普普通</Button>
+        <Button size="small" theme="link">小小小</Button>
+      </div>
+      <div>
+        <Button size="big" theme="text">大大大</Button>
+        <Button theme="text">普普通</Button>
+        <Button size="small" theme="text">小小小</Button>
+      </div>
+    </div>
+
+  <h1>示例3</h1>
+  <div>
+    <dir>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </dir>
+  </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
     size:{
       type:String,
       default:'normal'
-    }
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
 setup(props){
-    const {theme,size} = props
+    const {theme,size,level} = props
    const classes = computed(()=>{
 return {
   [`wuhu-theme-${theme}`]: theme,
-  [`wuhu-size-${size}`]: size
+  [`wuhu-size-${size}`]: size,
+  [`wuhu-level-${level}`]: level,
     }
    })
   return {classes}
@@ -38,6 +43,7 @@ $color: #333;
 $border-color: #d9d9d9;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .wuhu-button {
   box-sizing: border-box;
   height: $h;
@@ -52,6 +58,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -66,6 +73,24 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.wuhu-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.wuhu-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
   &.wuhu-size-big {
     font-size: 24px;
     height: 48px;
@@ -76,5 +101,56 @@ $radius: 4px;
     height: 20px;
     padding: 0 4px;
   }
+  &.wuhu-theme-button {
+    &.wuhu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.wuhu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.wuhu-theme-link {
+    &.wuhu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.wuhu-theme-text {
+    &.wuhu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.wuhu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 }
+
 </style>

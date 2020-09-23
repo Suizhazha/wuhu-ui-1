ff24d5e548178233859ffe9ed709793288f9671b
Suizhazha <573505403@qq.com>
2020-09-23 21:41:21 +0800

    修改button文字

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -16,19 +16,19 @@
   </h1>
     <div>
       <div>
-        <Button size="big">大大大</Button>
-         <Button>普普通</Button>
-        <Button size="small">小小小</Button>
+        <Button size="big">大大</Button>
+         <Button>普通</Button>
+        <Button size="small">小小</Button>
       </div>
       <div>
-        <Button theme="link" size="big">大大大</Button>
-        <Button theme="link">普普通</Button>
-        <Button size="small" theme="link">小小小</Button>
+        <Button theme="link" size="big">大大</Button>
+        <Button theme="link">普通</Button>
+        <Button size="small" theme="link">小小</Button>
       </div>
       <div>
-        <Button size="big" theme="text">大大大</Button>
-        <Button theme="text">普普通</Button>
-        <Button size="small" theme="text">小小小</Button>
+        <Button size="big" theme="text">大大</Button>
+        <Button theme="text">普通</Button>
+        <Button size="small" theme="text">小小</Button>
       </div>
     </div>
 

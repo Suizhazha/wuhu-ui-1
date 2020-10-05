e59e20bb2a613554f4f6dd694806a1adcc839870
Suizhazha <573505403@qq.com>
2020-10-05 14:22:54 +0800

    可以显示dialog页面，但是有bug

---
+++ src/components/dialog/DialogView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Dialog 示例</h1>
+  <Demo :component="Dialog1" />
+  <Demo :component="Dialog2" />
+</template>
+
+<script lang="ts">
+import Dialog2 from './Dialog2.vue';
+import Dialog1 from './Dialog1.vue';
+import Demo from '../Demo.vue';
+
+export default {
+  components: {
+    Demo
+  },
+  setup() {
+    return {
+      Dialog1,
+      Dialog2
+    }
+  }
+}
+</script>
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import SwitchView from './components/switch/SwitchView.vue';
-import ButtonDemo from './components/button/ButtonView.vue';
-import DialogDemo from "./components/dialog/DialogView.vue";
+import ButtonView from './components/button/ButtonView.vue';
+import DialogView from "./components/dialog/DialogView.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import Markdown from './components/Markdown.vue'
 import {h} from 'vue'
@@ -24,7 +24,8 @@ const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: fi
         {path: 'get-started',component: md('getStarted')},
         {path: 'install',component: md('install')},
         { path: "switch", component: SwitchView },
-        { path: "button", component: ButtonDemo },
+        { path: "button", component: ButtonView },
+        { path: "dialog", component: DialogView },
         { path: "tabs", component: TabsDemo }
       ]}
   ]

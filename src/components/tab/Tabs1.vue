ed4537056365b78b321cd60cc539d6274dda0fc8
Suizhazha <573505403@qq.com>
2020-10-05 15:51:58 +0800

    完善Tabs页面

---
+++ src/components/tab/Tabs1.vue
@@ -0,0 +1,30 @@
+<demo>
+常规使用
+</demo>
+<template>
+  <h1>示例</h1>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import {ref} from 'vue'
+import Tab from '../../lib/Tab.vue';
+import Tabs from '../../lib/Tabs.vue';
+
+export default {
+  components:{
+    Tabs,
+    Tab
+  },
+  setup(){
+    const x= ref('导航1')
+    return {
+      x
+    }
+  }
+}
+
+</script>
---
+++ src/components/tab/TabsView.vue
@@ -0,0 +1,20 @@
+<template>
+	<h1>Tabs 示例</h1>
+	<Demo :component="Tabs1" />
+</template>
+
+<script lang="ts">
+import Demo from '../Demo.vue';
+import Tabs1 from './Tabs1.vue';
+
+export default {
+  components: {
+    Demo
+  },
+  setup(){
+    return {
+      Tabs1
+    }
+  }
+}
+</script>
